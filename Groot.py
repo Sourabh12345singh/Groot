8385f0fa17e61c4cd32b83dab909ab5f043d8c13
Devyash Saini <dysaini2004@gmail.com>
2024-02-18 00:15:48 +0530

    Working Commit

--- Groot.py
+++ Groot.py
@@ -2,12 +2,15 @@ import os
 from dotenv import load_dotenv
 import json
 import openai
-from pinecone import Pinecone
+from pinecone import Pinecone, ServerlessSpec
+import logging
 
 load_dotenv()
 
 openai.api_key = os.environ.get("OPENAI_API_KEY")
 pinecone_api_key = os.environ.get("PINECONE_API_KEY")
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 chat = [
     {"role": "system", "content": "You are Groot, a RAG enhanced Large Language Model. You are like a virtual professor that can regularly learn new things. You are now not restricted to your training dataset. The RAG system will provide you with the reference information you need to answer question which are beyond your knowledge. If you get a Reference Information Along with the prompt, you need to use the given information along with your existing knowledge base (more emphasis on the provided reference). Your purpose is to provide the most accurate and relevant information to the student and help them in their learning journey."},
@@ -18,55 +21,62 @@ pinecone = Pinecone(api_key=pinecone_api_key)
 with open("Dataset/SourceMapping.json", "r") as f:
     sourceMapping = json.load(f)
 
-def getChunks(data, size=40):
+def getChunks(data, size=400):
     chunks = [data[i:i+size] for i in range(0, len(data), size)]
     return chunks
 
 def embedText(chunk):
     response = openai.embeddings.create(input=chunk,model="text-embedding-ada-002")
-    return response["data"][0]["embedding"]
+    return response.data[0].embedding
 
-def storeEmbeddings(embeddings, chunks):
+def storeEmbeddings(embeddings, chunks, file):
     vectors = []
 
-    index_name = 'Hack.IIITv.index'
+    index_name = 'hack-iiitv-index'
 
-    if index_name not in pinecone.list_indexes():
-        pinecone.create_index(index_name, dimension=1536)
+    indexes = pinecone.list_indexes()
+
+    indexes = [index.name for index in indexes]
+
+    if index_name not in indexes:
+        pinecone.create_index(index_name, dimension=1536, spec=ServerlessSpec( cloud="aws", region="us-west-2" ))
 
     index = pinecone.Index(index_name)
+    
+    lastChunkID = int(str(list(sourceMapping.keys())[-1])[-1]) if len(sourceMapping) > 0 else 0
 
-    lastChunkID= sourceMapping[-1].replace("chunk_","")
+    file = file.split(".")[0]
 
     for i, embedding in enumerate(embeddings):
-        chunk_id = f"chunk_{lastChunkID+i+1}"
+        chunk_id = f"{file.lower()}_chunk_{lastChunkID+i+1}"
         row = {}
         row['id'] = chunk_id
         row['values'] = embedding
         vectors.append(row)
 
     for i, chunk in enumerate(chunks):
-        sourceMapping[f"chunk_{lastChunkID+i+1}"] = chunk
+        sourceMapping[f"{file.lower()}_chunk_{lastChunkID+i+1}"] = chunk
 
     json.dump(sourceMapping, open("Dataset/SourceMapping.json", "w"))
-
+    logging.info(f"Source Mapping Updated...")
+    logging.info(f"Upserting Embeddings...")
     index.upsert(vectors=vectors)
 
-def processSample(data):
+def processSample(data,file):
     chunks = getChunks(data)
+
     embeddings = [embedText(chunk) for chunk in chunks]
-    storeEmbeddings(embeddings, chunks)
+    storeEmbeddings(embeddings, chunks, file)
 
 def queryDatabase(prompt):
     embeddings = embedText(prompt)
-
-    index_name = 'Hack.IIITv.index'
+    
+    index_name = 'hack-iiitv-index'
     index = pinecone.Index(index_name)
 
-    result = index.query(queries=embeddings, top_k=5)
-
-    matches = [sourceMapping[chunk["id"]] for chunk in result[matches]]
-
+    result = index.query(vector=embeddings, top_k=5)
+    
+    matches = [sourceMapping[chunk["id"]] for chunk in result["matches"]]
     return matches
 
 def generateResponse(referenceGranted, similarChunks, prompt):
@@ -81,15 +91,16 @@ def generateResponse(referenceGranted, similarChunks, prompt):
         """})
     else:
         chat.append({"role": "user", "content": prompt})
+
+    if len(chat) >= 9:
+        chat.remove(chat[1])
+        chat.remove(chat[1])
     
-    if len(chat) >= 6:
-        chat.remove(chat[0])
-    
-    response = openai.Completion.create(
+    response = openai.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=chat
     )
 
-    resp = response["choices"][0]["message"]["content"]
+    resp = response.choices[0].message.content
     chat.append({"role": "assistant", "content": resp})
     return resp
--- main.py
+++ main.py
@@ -1,23 +1,33 @@
 import os
 import shutil
 import Groot
+import logging
+from colorama import Fore, Back, Style
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 if __name__ == "__main__":
+    print(Fore.YELLOW, "\n  * Initializing Groot\n", Style.RESET_ALL)
     files = [f for f in os.listdir(os.path.join("Dataset")) if (os.path.isfile(os.path.join("Dataset",f)) and not f.endswith(".json"))]
+    if len(files) > 0:
+        logging.info(f"Files to process: {len(files)}")
     for file in files:
         with open(os.path.join("Dataset",file), "r") as f:
             Data = f.read()
-            Groot.processSample(Data)
+            Groot.processSample(Data,file)
         shutil.move(os.path.join("Dataset",file), os.path.join("Dataset/Embedded",file))
     
     while True:
+        print(Fore.YELLOW, "\n  * Groot is ready to chat. \n  * Type 'exit' to end the conversation.\n", Style.RESET_ALL)
         prompt = input("User: ")
         if prompt == "exit":
             break
         if "<//>" not in prompt:
             similarChunks = Groot.queryDatabase(prompt)
+            print("Similar Chunks: ", similarChunks)
             resp = Groot.generateResponse(True, similarChunks, prompt)
         else:
+            prompt.replace("<//>","")
             resp = Groot.generateResponse(False, [], prompt)
 
         print("Groot: ", resp)
